60e4e873f4ab4f5a44128ddb761862d83273d67d
Santo120199 <santo120199@gmail.com>
2021-11-23 16:41:19 +0100

    From now serius commit

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,7 +4,28 @@ import { PageHero } from '../components'
 import aboutImg from '../assets/hero-bcg.jpeg'
 
 const AboutPage = () => {
-  return <h4>about page</h4>
+  return (
+    <main>
+      <PageHero title='about' />
+      <Wrapper className='page section section-center'>
+        <img src={aboutImg} alt='nice desk' />
+        <article>
+          <div className='title'>
+            <h2>our story</h2>
+            <div className='underline'></div>
+          </div>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque
+            sint magni repudiandae aliquid animi fugiat assumenda provident
+            tempora aliquam dolor. Culpa eaque doloribus repellendus sequi id
+            error quidem qui laboriosam dolores reprehenderit voluptate rerum
+            hic, exercitationem nemo voluptatem sit minus corporis modi libero
+            tempora. Modi ad nemo reiciendis quae excepturi!
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  )
 }
 
 const Wrapper = styled.section`
